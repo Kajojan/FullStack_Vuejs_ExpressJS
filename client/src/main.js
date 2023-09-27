a5f02a8ddd73864d533d6f890d5cc258a2d06e13
Kajojan <kojoty1999@gmail.com>
2023-09-27 19:11:23 +0200

    changes related to the store

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,26 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { sync } from 'vuex-router-sync'
 import App from './App.vue'
 import router from './router'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import store from './stores/store'
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
+  components,
+  directives
+})
 
 const app = createApp(App)
-
 app.use(vuetify)
-// app.use(createPinia())
+app.use(createPinia())
 app.use(router)
+app.use(store)
+
+// sync(store, router)
 
 app.mount('#app')
