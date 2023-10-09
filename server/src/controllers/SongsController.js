4404855f3b38a5f40e9df5e20e373c2715092e86
Kajojan <kojoty1999@gmail.com>
2023-10-09 21:27:28 +0200

    adding search filter by  song title, artist, album, or genre

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -1,57 +1,77 @@
 const { Song } = require("../models");
+const { Op } = require('sequelize');
+
 
 module.exports = {
   async getAllSong(req, res) {
     try {
-      const song = await Song.findAll({
-        limit:10
-      })
-      res.send(song)
+      let songs = null
+      console.dir(songs);
+      const search = req.query.search
+      console.dir(search);
+      if (search) {
+        songs = await Song.findAll({
+          where: {
+            [Op.or]: [
+              { title: { [Op.like]: `%${search}%` } },
+              { artist: { [Op.like]: `%${search}%` } },
+              { genre: { [Op.like]: `%${search}%` } },
+              { album: { [Op.like]: `%${search}%` } }
+            ]
+          }
+        })
+        
+      console.dir(songs);
+
+      } else {
+        songs = await Song.findAll({
+          limit: 10
+        })
+      }
+      res.send(songs);
     } catch (err) {
+      console.log(err);
       res.status(500).send({
-        error: 'An error has accoured trying to log in'
-      })
+        error: "An error has accoured trying to log in",
+      });
     }
   },
   async postNewSong(req, res) {
     try {
-      const song = await Song.create(req.body)
-      res.send(song)
+      const song = await Song.create(req.body);
+      res.send(song);
     } catch (err) {
       res.status(500).send({
-
-        error: 'An error has accoured trying to log in'
-      })
+        error: "An error has accoured trying to log in",
+      });
     }
   },
-  async show(req,res){
-    try{
-      const song = await Song.findByPk(req.params.songId) //Sequelize findByPk = findById() 
-      res.send(song)
-
-    }catch(err){
+  async show(req, res) {
+    try {
+      const song = await Song.findByPk(req.params.songId); //Sequelize findByPk = findById()
+      res.send(song);
+    } catch (err) {
       console.dir(err);
 
       res.status(500).send({
-        error: 'An error has accoured trying to log in'
-      })   
-     }
+        error: "An error has accoured trying to log in",
+      });
+    }
   },
-  async edit (req, res) {
+  async edit(req, res) {
     try {
       const song = await Song.update(req.body, {
         where: {
-          id: req.params.songId
-        }
-      })
-      
+          id: req.params.songId,
+        },
+      });
+
       console.log(req.body);
-      res.send(req.body)
+      res.send(req.body);
     } catch (err) {
       res.status(500).send({
-        error: 'an error has occured trying to update the song'
-      })
+        error: "an error has occured trying to update the song",
+      });
     }
-  }
-
-}
+  },
+};
