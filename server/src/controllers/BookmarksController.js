10e2d4aec41d3608e321855907b360aba2b19f39
Kajojan <kojoty1999@gmail.com>
2023-11-04 20:34:19 +0100

    bookmarkControllers func index changed related to tabel of bookmarks

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -1,65 +1,71 @@
-const {
-    Bookmark,
-    Song,
-    User
-  } = require('../models')
-  
-  module.exports = {
-    async index (req, res) {
-      try {
-        const {songId, userId} = req.query
-        const bookmark = await Bookmark.findOne({
-          where: {
-            SongId: songId,
-            UserId: userId
-          }
-        })
-        res.send(bookmark)
-      } catch (err) {
-        console.log(err);
-        res.status(500).send({
-          error: 'an error has occured trying to fetch the bookmark'
-        })
+const { Bookmark, Song, User } = require("../models");
+const _ = require('lodash')
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const { songId, userId } = req.query;
+      console.log(req.query);
+      const where = {
+        UserId: userId,
+      };
+      if (songId) {
+        where.SongId = songId;
       }
-    },
-    async post (req, res) {
-      try {
-        const {songId, userId} = req.body
-        const bookmark = await Bookmark.findOne({
-          where: {
-            SongId: songId,
-            UserId: userId
-          }
-        })
-        if (bookmark) {
-          return res.status(400).send({
-            error: 'you already have this set as a bookmark'
-          })
-        }
-        const newBookmark = await Bookmark.create(req.body)
-        const song = await Song.findByPk(songId)
-        const user = await User.findByPk(userId)
-        await newBookmark.setUser(user)
-        await newBookmark.setSong(song)
-        res.send(newBookmark)
-      } catch (err) {
-        console.log(err)
-        res.status(500).send({
-          error: 'an error has occured trying to create the bookmark'
-        })
-      }
-    },
-    async delete (req, res) {
-      try {
-        const {bookmarkId} = req.params
-        const bookmark = await Bookmark.findByPk(bookmarkId)
-        await bookmark.destroy()
-        res.send(bookmark)
-      } catch (err) {
-        console.log(err);
-        res.status(500).send({
-          error: 'an error has occured trying to delete the bookmark'
-        })
-      }
+      const bookmark = await Bookmark.findAll({
+        where: where,
+        include: [{ model: Song }],
+      })
+      bookmark.map((bookmark) => bookmark.toJSON())
+      .map((bookmark) => _.extend({}, bookmark.Song, bookmark));
+      console.log('bookmarks: ' , bookmark);
+
+      res.send(bookmark);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error: "an error has occured trying to fetch the bookmark",
+      });
     }
-  }
+  },
+  async post(req, res) {
+    try {
+      const { songId, userId } = req.body;
+      const bookmark = await Bookmark.findOne({
+        where: {
+          SongId: songId,
+          UserId: userId,
+        },
+      });
+      if (bookmark) {
+        return res.status(400).send({
+          error: "you already have this set as a bookmark",
+        });
+      }
+      const newBookmark = await Bookmark.create(req.body);
+      const song = await Song.findByPk(songId);
+      const user = await User.findByPk(userId);
+      await newBookmark.setUser(user);
+      await newBookmark.setSong(song);
+      res.send(newBookmark);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error: "an error has occured trying to create the bookmark",
+      });
+    }
+  },
+  async delete(req, res) {
+    try {
+      const { bookmarkId } = req.params;
+      const bookmark = await Bookmark.findByPk(bookmarkId);
+      await bookmark.destroy();
+      res.send(bookmark);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error: "an error has occured trying to delete the bookmark",
+      });
+    }
+  },
+};
