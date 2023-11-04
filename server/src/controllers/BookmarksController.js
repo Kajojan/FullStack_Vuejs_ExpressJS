14adf52f297e6d91c984b0a2cfd5f1a10665bb86
Kajojan <kojoty1999@gmail.com>
2023-11-04 18:13:18 +0100

    server bookmark Controllers

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -37,8 +37,8 @@ const {
           })
         }
         const newBookmark = await Bookmark.create(req.body)
-        const song = await Song.findById(songId)
-        const user = await User.findById(userId)
+        const song = await Song.findByPk(songId)
+        const user = await User.findByPk(userId)
         await newBookmark.setUser(user)
         await newBookmark.setSong(song)
         res.send(newBookmark)
@@ -52,10 +52,11 @@ const {
     async delete (req, res) {
       try {
         const {bookmarkId} = req.params
-        const bookmark = await Bookmark.findById(bookmarkId)
+        const bookmark = await Bookmark.findByPk(bookmarkId)
         await bookmark.destroy()
         res.send(bookmark)
       } catch (err) {
+        console.log(err);
         res.status(500).send({
           error: 'an error has occured trying to delete the bookmark'
         })
